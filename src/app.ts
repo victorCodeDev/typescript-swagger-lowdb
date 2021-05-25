9ad3af33574adfb61ba374d60507a3dc1bca309a
victor.ojeda <ojedavictor80@gmail.com>
2021-05-24 21:25:36 -0400

    Se termina de realizar las implementaciones para el crud de clientes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
+
 import clienteRoutes from './routes/cliente.routes';
 
 const app = express();
@@ -11,5 +14,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(clienteRoutes);
+app.use('/docs', swaggerUI.serve);
 
 export default app;
--- src/controllers/cliente.controllers.ts
+++ src/controllers/cliente.controllers.ts
@@ -3,15 +3,23 @@ import { getConnection } from '../db'
 import { nanoid } from 'nanoid';
 
 export const getClientes: Handler = (req, res) => {
-    const data = getConnection().get('clientes').value();
-    return res.json(data);
+    const clientes = getConnection().get('clientes').value();
+    return res.json(clientes);
 }
 
 export const getClientesCount: Handler = (req, res) => {
-
+    let cantidad = getConnection().get('clientes').value().length;
+    res.json(cantidad);
 }
 
-export const getClienteById: Handler = (req, res) => res.send('get cliente by id')
+export const getClienteById: Handler = (req, res) => {
+
+    const cliente = getConnection().get('clientes').find({ id: req.params.id }).value();
+    if (!cliente) {
+        res.status(404).json({ msg: 'Cliente no encontrado' })
+    }
+    return res.json(cliente);
+}
 
 export const createCliente: Handler = (req, res) => {
     const { nombre, apellido, numeroDocumento } = req.body;
@@ -31,8 +39,23 @@ export const createCliente: Handler = (req, res) => {
 
 }
 
-export const updateCliente: Handler = (req, res) => res.send('update cliente')
+export const updateCliente: Handler = (req, res) => {
+    const cliente = getConnection().get('clientes').find({ id: req.params.id }).value();
+    if (!cliente) {
+        res.status(404).json({ msg: 'Cliente no encontrado' })
+    }
+    const clienteModificado = getConnection().get('clientes').find({ id: req.params.id }).assign(req.body).write();
+    res.json(clienteModificado);
+}
 
-export const deleteCliente: Handler = (req, res) => res.send('delete cliente')
+export const deleteCliente: Handler = (req, res) => {
+    const cliente = getConnection().get('clientes').find({ id: req.params.id }).value();
+    if (!cliente) {
+        res.status(404).json({ msg: 'Cliente no encontrado' })
+    }
+    const clienteEliminado = getConnection().get('clientes').remove({ id: req.params.id }).write();
+    
+    res.json(clienteEliminado);
+}
 
 
