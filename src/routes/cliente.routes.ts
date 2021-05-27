135fcb89467f08a757adc77e6e9bf117a1e4d675
victor.ojeda <ojedavictor80@gmail.com>
2021-05-26 23:28:49 -0400

    Se grega documentación para api get de clientes

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -2,6 +2,53 @@ import { Router } from 'express';
 import { getClientes, getClientesCount, getClienteById, createCliente, updateCliente, deleteCliente } from '../controllers/cliente.controllers';
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Cliente:
+ *          Type: object
+ *          properties:
+ *              id:
+ *                  type: string
+ *                  description: 'Id autogenerado para el cliente'
+ *              nombre:
+ *                  type: string
+ *                  description: 'Nombre del cliente'
+ *              apellido:
+ *                  type: string
+ *                  description: 'Apellido del cliente'
+ *              numeroDocumento:
+ *                  type: string
+ *                  description: 'Número de Documento del cliente'
+ *          required:
+ *              - nombre
+ *              - apellido
+ *              - numeroDocumento
+ *          example:
+ *              id: 11111
+ *              nombre: Juan
+ *              apellido: Perez
+ *              numeroDocumento: 2135707
+ */
+
+/**
+ * @swagger
+ * /clientes:
+ *  get:
+ *    summary: Returns a list of tasks
+ *    tags: [Tasks]
+ *    responses:
+ *      200:
+ *        description: the list of tasks
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Cliente'
+ */
+
 router.get('/clientes', getClientes);
 router.get('/clientes/count', getClientesCount);
 router.get('/cliente/:id', getClienteById);
